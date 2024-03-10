7e348982a03f1ff4e365aacca0a3ceb5a03ac2d1
derbuihan <derbuihan@gmail.com>
2024-03-10 11:10:40 +0900

    Implemented low level disk functionality in C

--- src/kernel.c
+++ src/kernel.c
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "disk/disk.h"
 #include "idt/idt.h"
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
@@ -76,19 +77,11 @@ void kernel_main() {
   // Switch to kernel paging chunk
   paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
 
-  char *ptr = kzalloc(4096);
-  paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void *)0x1000,
-             (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT |
-                 PAGING_IS_WRITABLE);
-
   // Enable paging
   enable_paging();
 
-  char *ptr2 = (char *)0x1000;
-  ptr2[0] = 'a';
-  ptr2[1] = 'b';
-  print(ptr2);
-  print(ptr);
+  char buf[512];
+  disk_read_sector(0, 1, buf);
 
   // Enable interrupts
   enable_interrupts();
